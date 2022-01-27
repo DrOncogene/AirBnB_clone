45c2986674a9ee63f8c81148ee9501a333bed0c1
Charles-Homks <charles01homks@gmail.com>
2022-01-27 11:23:04 -0800

    Other classes that inherit from BaseModel

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Amenity class that inherit from Basemodel"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Simple Amenity model"""
+    name = ""
